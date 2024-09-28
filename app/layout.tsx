63539b5c96e2e8e394cb9bea3585959683f7fd9d
Olayemi Obagboye <richardoo@live.com>
2024-09-28 16:11:11 +0200

    Implement the complete layout skeleton

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,16 +11,16 @@ const ibmPlexSerif = IBM_Plex_Serif({
 })
 
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata: Metadata = {
   title: "Horizon",
